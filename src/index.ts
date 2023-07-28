4f38c6f266a10875b62972c9a0aa1565bfc9ec6a
Simon Renoult <simon_renoult@connect-tech.sncf>
2023-07-28 15:12:37 +0200

    chore: use nest-cls

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ClsModule } from "nestjs-cls";
 
 import { CarboneModule } from "./carbone";
 import { MiddlewaresModule } from "./shared/middlewares";
 
 @Module({
-  imports: [MiddlewaresModule, CarboneModule],
+  imports: [
+    ClsModule.forRoot({
+      global: true,
+      middleware: { mount: true },
+    }),
+    MiddlewaresModule,
+    CarboneModule,
+  ],
 })
 export class TortueModule {}
--- src/shared/middlewares/index.ts
+++ src/shared/middlewares/index.ts
@@ -1,78 +1,22 @@
-import { AsyncLocalStorage } from "node:async_hooks";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { ClsStore } from "nestjs-cls";
 
-import {
-  Injectable,
-  MiddlewareConsumer,
-  Module,
-  NestMiddleware,
-  NestModule,
-} from "@nestjs/common";
-import { NextFunction, Request, Response } from "express";
-import { ulid } from "ulid";
+import { AddTracingIdentifiersToContextMiddleware } from "./add-tracing-identifiers-to-context.middleware";
+import { LogIncomingRequestMiddleware } from "./log-incoming-request.middleware";
 
-interface RequestContext {
-  [key: string]: string;
+export interface Context extends ClsStore {
+  correlationId: string;
+  requestId: string;
 }
 
-@Module({
-  providers: [
-    {
-      provide: AsyncLocalStorage,
-      useValue: new AsyncLocalStorage(),
-    },
-  ],
-})
+@Module({})
 export class MiddlewaresModule implements NestModule {
-  constructor(private readonly als: AsyncLocalStorage<RequestContext>) {}
-
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
-        (req: Request, res: Response, next: NextFunction) =>
-          this.als.run({}, () => next()),
-        InitializeCorrelationIdMiddleware,
-        InitializeRequestIdMiddleware,
+        AddTracingIdentifiersToContextMiddleware,
         LogIncomingRequestMiddleware,
       )
       .forRoutes("*");
-  }
-}
-
-const getStore = (als: AsyncLocalStorage<RequestContext>): RequestContext => {
-  const store = als.getStore();
-  if (!store) throw new Error("No store");
-  return store;
-};
-
-@Injectable()
-class InitializeCorrelationIdMiddleware implements NestMiddleware {
-  constructor(private readonly als: AsyncLocalStorage<RequestContext>) {}
-
-  use(req: Request, res: Response, next: NextFunction) {
-    const correlationId = (req.headers["X-Correlation-Id"] as string) ?? ulid();
-    getStore(this.als).correlationId = correlationId;
-    return next();
-  }
-}
-
-@Injectable()
-class InitializeRequestIdMiddleware implements NestMiddleware {
-  constructor(private readonly als: AsyncLocalStorage<RequestContext>) {}
-
-  use(req: Request, res: Response, next: NextFunction) {
-    const requestId = (req.headers["X-Request-Id"] as string) ?? ulid();
-    getStore(this.als).requestId = requestId;
-    return next();
-  }
-}
-
-@Injectable()
-class LogIncomingRequestMiddleware implements NestMiddleware {
-  constructor(private readonly als: AsyncLocalStorage<RequestContext>) {}
-
-  use(req: Request, res: Response, next: NextFunction) {
-    const correlationdId = getStore(this.als).correlationId;
-    console.log(correlationdId, res.statusCode, req.method, req.url);
-    return next();
   }
 }
